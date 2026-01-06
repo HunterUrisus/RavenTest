037ac2b674636ed1496a3f1a1a0706b1a79cf8be
Olimpo Server <olimpo.sx2022@gmail.com>
2026-01-06 19:37:46 +0000

    Server update

--- frontend/src/services/root.service.js
+++ frontend/src/services/root.service.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
-
 const instance = axios.create({
-    baseURL: API_URL,
+    baseURL: '/api',
     headers: {
         "Content-Type": "application/json",
     },
